8a876ef5cbcede78cc3d351fca6d6d72130c65c9
Maciej M <michalowski.ma@gmail.com>
2017-12-31 19:07:14 +0100

    Update sort.sh

--- scripts/sort.sh
+++ scripts/sort.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+rm adblock_posortowany.txt
 
 for (( i=1; $i <= 37; i++ )) ; do
 
-	let poczatek=i
-	let koniec=i+1
-	
-	poczatek1=`cat -n adblock.txt | grep "!$poczatek#" | awk '{print $1;}'`
-	koniec1=`cat -n adblock.txt | grep "!$koniec#" | awk '{print $1;}'`
-	
-	let koniec2=(koniec1-1)
-	
-	sed -n "${poczatek1},${koniec2}p" adblock.txt > sekcja$poczatek.txt
-	sort -u sekcja$poczatek.txt > sekcja_po$poczatek.txt
+        let poczatek=i
+        let koniec=i+1
+
+        poczatek1=`cat -n adblock.txt | grep "!$poczatek#" | awk '{print $1;}'`
+        koniec1=`cat -n adblock.txt | grep "!$koniec#" | awk '{print $1;}'`
+
+        let koniec2=(koniec1-1)
+
+        sed -n "${poczatek1},${koniec2}p" adblock.txt > sekcja$poczatek.txt
+        sort -u sekcja$poczatek.txt > sekcja_po$poczatek.txt
+        cat sekcja_po$poczatek.txt >> adblock_posortowany.txt
 
 done
