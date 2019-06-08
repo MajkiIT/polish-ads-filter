233277e426968c5742040b52b9f58ce54f37e1e4
Maciej M <michalowski.ma@gmail.com>
2019-06-08 12:57:58 +0200

    Create polish_ads_wp.js

---
+++ polish-adblock-filters/polish_ads_wp.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Reklamy WP
+// @namespace    .......
+// @version      0.1
+// @description  Blokowanie reklam na wp.pl
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  var stronyWP = ["abczdrowie.pl", "allani.pl", "autokrata.pl", "autokult.pl", "biztok.pl", "www.dobreprogramy.pl", "dzieci.pl", "easygo.pl", "echirurgia.pl", "fotoblogia.pl", "gadzetomania.pl", "hotmoney.pl", "interaktywnie.com", "inwestycje.pl", "jejswiat.pl", "kafeteria.pl", "kafeteria.tv", "kantory.pl", "kazimierzdolny.pl", "komediowo.pl", "komorkomania.pl", "mazury.com", "mojeauto.pl", "mojeosiedle.pl", "nasygnale.pl", "niewiarygodne.pl", "nocowanie.pl", "o2.pl", "odkrywcy.pl", "open.fm", "parenting.pl", "pudelek.pl", "pudelek.tv", "pytamy.pl", "sfora.pl", "snobka.pl", "tlen.pl", "totalmoney.pl", "wakacje.pl", "wawalove.pl", "wp.tv"];
+
+  for (var i = 0; i < stronyWP.length; i++) {
+    if (window.location.hostname === stronyWP[i] || window.location.hostname.indexOf(".wp.pl") != -1) {
+      Object.defineProperty(Object.prototype, 'bodies', {
+        get: function get() {
+          return function () {};
+        },
+        set: function set() {
+          return function () {};
+        }
+      });
+    }
+  }
+})();
