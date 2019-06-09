b5f3c75ea34eaacdb50e7e0031efc22512cfc0af
Maciej M <michalowski.ma@gmail.com>
2019-06-09 10:13:47 +0200

    Update polish_ads_wp.user.js

--- polish-adblock-filters/polish_ads_wp.user.js
+++ polish-adblock-filters/polish_ads_wp.user.js
@@ -1,30 +1,65 @@
 // ==UserScript==
 // @name         Reklamy WP
 // @namespace    F4z
-// @version      0.2
+// @version      0.3
 // @description  Blokowanie reklam na wp.pl
 // @author       You
-// @match        *://*/*
+// @match        *://*.abczdrowie.pl/*
+// @match        *://*.allani.pl/*
+// @match        *://*.autokrata.pl/*
+// @match        *://*.autokult.pl/*
+// @match        *://*.biztok.pl/*
+// @match        *://*.www.dobreprogramy.pl/*
+// @match        *://*.dzieci.pl/*
+// @match        *://*.easygo.pl/*
+// @match        *://*.echirurgia.pl/*
+// @match        *://*.fotoblogia.pl/*
+// @match        *://*.gadzetomania.pl/*
+// @match        *://*.hotmoney.pl/*
+// @match        *://*.interaktywnie.com/*
+// @match        *://*.inwestycje.pl/*
+// @match        *://*.jejswiat.pl/*
+// @match        *://*.kafeteria.pl/*
+// @match        *://*.kafeteria.tv/*
+// @match        *://*.kantory.pl/*
+// @match        *://*.kazimierzdolny.pl/*
+// @match        *://*.komediowo.pl/*
+// @match        *://*.komorkomania.pl/*
+// @match        *://*.mazury.com/*
+// @match        *://*.mojeauto.pl/*
+// @match        *://*.mojeosiedle.pl/*
+// @match        *://*.nasygnale.pl/*
+// @match        *://*.niewiarygodne.pl/*
+// @match        *://*.nocowanie.pl/*
+// @match        *://*.o2.pl/*
+// @match        *://*.odkrywcy.pl/*
+// @match        *://*.open.fm/*
+// @match        *://*.parenting.pl/*
+// @match        *://*.pudelek.pl/*
+// @match        *://*.pudelek.tv/*
+// @match        *://*.pytamy.pl/*
+// @match        *://*.sfora.pl/*
+// @match        *://*.snobka.pl/*
+// @match        *://*.tlen.pl/*
+// @match        *://*.totalmoney.pl/*
+// @match        *://*.wakacje.pl/*
+// @match        *://*.wawalove.pl/*
+// @match        *://*.wp.tv/*
+// @match        *://*.wp.pl/*
 // @grant        none
-// @updateURL https://raw.githubusercontent.com/MajkiIT/polish-ads-filter/master/polish-adblock-filters/polish_ads_wp.user.js
-// @downloadURL https://raw.githubusercontent.com/MajkiIT/polish-ads-filter/master/polish-adblock-filters/polish_ads_wp.user.js
+// @updateURL    https://raw.githubusercontent.com/MajkiIT/polish-ads-filter/master/polish-adblock-filters/polish_ads_wp.user.js
+// @downloadURL  https://raw.githubusercontent.com/MajkiIT/polish-ads-filter/master/polish-adblock-filters/polish_ads_wp.user.js
 // ==/UserScript==
-
+ 
 (function () {
-  'use strict';
-
-  var stronyWP = ["abczdrowie.pl", "allani.pl", "autokrata.pl", "autokult.pl", "biztok.pl", "www.dobreprogramy.pl", "dzieci.pl", "easygo.pl", "echirurgia.pl", "fotoblogia.pl", "gadzetomania.pl", "hotmoney.pl", "interaktywnie.com", "inwestycje.pl", "jejswiat.pl", "kafeteria.pl", "kafeteria.tv", "kantory.pl", "kazimierzdolny.pl", "komediowo.pl", "komorkomania.pl", "mazury.com", "mojeauto.pl", "mojeosiedle.pl", "nasygnale.pl", "niewiarygodne.pl", "nocowanie.pl", "o2.pl", "odkrywcy.pl", "open.fm", "parenting.pl", "pudelek.pl", "pudelek.tv", "pytamy.pl", "sfora.pl", "snobka.pl", "tlen.pl", "totalmoney.pl", "wakacje.pl", "wawalove.pl", "wp.tv"];
-
-  for (var i = 0; i < stronyWP.length; i++) {
-    if (window.location.hostname === stronyWP[i] || window.location.hostname.indexOf(".wp.pl") != -1) {
-      Object.defineProperty(Object.prototype, 'bodies', {
+    'use strict';
+ 
+    Object.defineProperty(Object.prototype, 'bodies', {
         get: function get() {
-          return function () {};
+            return function () {};
         },
         set: function set() {
-          return function () {};
+            return function () {};
         }
-      });
-    }
-  }
+    });
 })();
