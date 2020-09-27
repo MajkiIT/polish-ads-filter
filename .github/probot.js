2e11221672596323d853cd9d0976cee1d75aa598
hawkeye116477 <hawkeye116477@gmail.com>
2020-09-27 19:30:51 +0200

    Revert "Add new awesome bot"

---
+++ .github/probot.js
@@ -0,0 +1,8 @@
+on('issues.opened')
+  .comment(`
+    Hello @{{ sender.login }}. Thanks for inviting me to your project.
+    Read more about [all the things I can help you with][config]. I can't
+    wait to get started!
+
+    [config]: https://github.com/bkeepers/workflow/blob/master/docs/configuration.md
+  `);
